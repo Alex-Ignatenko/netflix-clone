53f3192377aa0166ff798b4015ca35c9cc2ae15d
Alex Ignatenko <alex80850@gmail.com>
2023-08-16 11:21:34 +0300

    c

--- Client/src/components/Slider/Slider.jsx
+++ Client/src/components/Slider/Slider.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import SliderItem from '../SliderItem/SliderItem'
 import "./Slider.scss"
 
 const Slider = () => {
+
+    
 
     const responsive = {
         superLargeDesktop: {
           // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
-          items: 7,
+          items: 5,
           slidesToSlide: 4
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
+          breakpoint: { max: 3000, min: 1500 },
           items: 5,
           slidesToSlide: 3
         },
         tablet: {
-          breakpoint: { max: 1024, min: 480 },
+          breakpoint: { max: 1500, min: 480 },
           items: 3,
           slidesToSlide: 2
         },
@@ -32,16 +35,17 @@ const Slider = () => {
   return (
     <div className='slider-container'>
         <div><h2>List Name</h2></div>
-        <Carousel className="slider-sub-container"responsive={responsive} infinite={true} centerMode ={true} swipeable={true} draggable={true}>
-          <div className='slider-item'>Item 1</div>
-          <div className='slider-item'>Item 2</div>
-          <div className='slider-item'>Item 3</div>
-          <div className='slider-item'>Item 4</div>
-          <div className='slider-item'>Item 5</div>
-          <div className='slider-item'>Item 6</div>
-          <div className='slider-item'>Item 7</div>
-          <div className='slider-item'>Item 8</div>
-      </Carousel>
+          <Carousel className="slider-sub-container"responsive={responsive} infinite={true} centerMode ={true} swipeable={true} draggable={true}>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+            <SliderItem className='slider-item'></SliderItem>
+
+        </Carousel>
     </div>
   )
 }
