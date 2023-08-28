cb79ac7a91f4cd62d11a31937ce5b3e00e6d3655
Alex Ignatenko <alex80850@gmail.com>
2023-08-28 16:54:45 +0300

    merge

--- Client/src/components/Searchbox/Searchbox.jsx
+++ Client/src/components/Searchbox/Searchbox.jsx
@@ -1,44 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
-import { GetURLSearchFilter } from '../../Services/GetURLSearchFilter';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { GetURLSearchFilter } from "../../Services/GetURLSearchFilter";
 
-import "./Searchbox.scss"
+import "./Searchbox.scss";
 
-const Searchbox = ({showSearch, isToggleable}) => {
-    const [query, setQuery] = useState("");
-    const [isSearchShown, setisSearchShown] = useState(showSearch);
-    const {search, pathname} = useLocation();
-    const navigate = useNavigate();
+const Searchbox = ({ showSearch, isToggleable }) => {
+  const [query, setQuery] = useState("");
+  const [isSearchShown, setisSearchShown] = useState(showSearch);
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
 
-    console.log("search:" + search)
+  if (isToggleable) {
+  }
+  const toggleSearchbar = () => {
+    setisSearchShown(!isSearchShown);
+  };
 
-    if(isToggleable){
-      
+  //Enables search as you type via use effect that fires every query change
+  useEffect(() => {
+    if (pathname != "/search" && !query) {
+      return;
     }
-    const toggleSearchbar = () => {
-        setisSearchShown(!isSearchShown)
-    }
-   
-   //Enables search as you type via use effect that fires every query change
-    useEffect(() =>{
-       if(pathname != '/search' && !query){
-           return;
-       };
-       console.log("query:" + query);
-       const link = GetURLSearchFilter(search, {query: query || ''});  
-       navigate(link);
-   
-    },[query]);
-   
+    const link = GetURLSearchFilter(search, { query: query || "" });
+    navigate(link);
+  }, [query]);
 
   return (
-    <div className={isSearchShown ? "my-search-container shown" : "my-search-container"}>
-        <div className={isToggleable? "icon-container" : ""}>
-          <span  onClick={isToggleable? toggleSearchbar : undefined} className="material-symbols-outlined my-icon">Search</span>         
-        </div>
-        <input type='text' onChange={(e) => setQuery(e.target.value)} placeholder='Search..,' className={isSearchShown ? 'my-search-input shown' : 'my-search-input'}/> 
+    <div
+      className={
+        isSearchShown ? "my-search-container shown" : "my-search-container"
+      }
+    >
+      <div className={isToggleable ? "icon-container" : ""}>
+        <span
+          onClick={isToggleable ? toggleSearchbar : undefined}
+          className="material-symbols-outlined my-icon"
+        >
+          Search
+        </span>
+      </div>
+      <input
+        type="text"
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search..,"
+        className={isSearchShown ? "my-search-input shown" : "my-search-input"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Searchbox
+export default Searchbox;
