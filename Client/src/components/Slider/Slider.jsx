badf29e83ef5728a5d87421adecdb9e9b4d62706
Dvir Adani <dvirdvirdvir21@gmail.com>
2023-08-28 16:31:47 +0300

    search-page-works

--- Client/src/components/Slider/Slider.jsx
+++ Client/src/components/Slider/Slider.jsx
@@ -1,34 +1,30 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination , Navigation , Scrollbar , A11y} from 'swiper/modules';
+import React, { useContext, useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Scrollbar, A11y } from "swiper/modules";
 import { authContext } from "../../context/authContext";
-import axios from 'axios';
-import SliderItem from '../SliderItem/SliderItem';
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-import "./Slider.scss"
+import axios from "axios";
+import SliderItem from "../SliderItem/SliderItem";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "./Slider.scss";
 
-
-
-const Slider = ({type , genre, title}) => {
-
+const Slider = ({ type, genre, title }) => {
   const [contents, setContents] = useState([]);
-  const {userInfo, dispatch} = useContext(authContext);
-
+  const { userInfo, dispatch } = useContext(authContext);
 
   useEffect(() => {
     const getList = async () => {
-      try {    
-        let requestedType = type ? '?type=' +  type : '?type=all';
-        let requestedGenre = genre ? '&genre=' +  genre : '';
-        let path = '/content/getlist' + requestedType + requestedGenre;
+      try {
+        let requestedType = type ? "?type=" + type : "?type=all";
+        let requestedGenre = genre ? "&genre=" + genre : "";
+        let path = "/content/getlist" + requestedType + requestedGenre;
         const responce = await axios.get(path, {
           headers: {
             authorization: userInfo.token,
           },
         });
-        if (responce){
+        if (responce) {
           setContents(responce.data);
         }
       } catch (error) {
@@ -36,66 +32,64 @@ const Slider = ({type , genre, title}) => {
       }
     };
     getList();
-   
   }, [type]);
-
-
 
   return (
     <div className="slider-container">
       <h1>{title}</h1>
       <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      breakpoints={{
-        // when window width is >= 480px
-            480: {
-              slidesPerView: 1,
-            },
-        // when window width is >= 768px
-            500: {
-              spaceBetween: 4,
-              slidesPerGroup: 2,
-              slidesPerView: 2,
-            },
-        // when window width is >= 1024px
-            1024: {
-              spaceBetween: 6,
-              slidesPerGroup: 3,
-              slidesPerView: 3,
-            },
-            1280: {
-              spaceBetween: 8,
-              slidesPerGroup: 4,
-              slidesPerView: 4,
-            },
-            1480: {
-              spaceBetween: 10,
-              slidesPerGroup: 5,
-              slidesPerView: 5,
-            },
-            // 1880: {
-            //   spaceBetween: 12,
-            //   slidesPerGroup: 6,
-            //   slidesPerView: 6,
-            // }
-          }}
-      navigation={{
-        // nextEl: '.swiper-button-next',
-        // prevEl: '.swiper-button-prev',
-      disabledClass: 'disabled_swiper_button'}}    
-      // pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      loop={true}
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        breakpoints={{
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 1,
+          },
+          // when window width is >= 768px
+          500: {
+            spaceBetween: 4,
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+          },
+          // when window width is >= 1024px
+          1024: {
+            spaceBetween: 6,
+            slidesPerGroup: 3,
+            slidesPerView: 3,
+          },
+          1280: {
+            spaceBetween: 8,
+            slidesPerGroup: 4,
+            slidesPerView: 4,
+          },
+          1480: {
+            spaceBetween: 10,
+            slidesPerGroup: 5,
+            slidesPerView: 5,
+          },
+          // 1880: {
+          //   spaceBetween: 12,
+          //   slidesPerGroup: 6,
+          //   slidesPerView: 6,
+          // }
+        }}
+        navigation={{
+          // nextEl: '.swiper-button-next',
+          // prevEl: '.swiper-button-prev',
+          disabledClass: "disabled_swiper_button",
+        }}
+        // pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        loop={true}
       >
-        {contents.map((content) => (
-            <SwiperSlide>
-              <SliderItem content = {content}/>
-            </SwiperSlide>
+        {contents.map((content, i) => (
+          <SwiperSlide key={i}>
+            <SliderItem content={content} />
+          </SwiperSlide>
         ))}
-      </Swiper> 
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
--- Server/Services/GetSearchFilter.js
+++ Server/Services/GetSearchFilter.js
@@ -1,30 +1,27 @@
-
 const GetSearchFilter = (query) => {
+  const searchQuery = query.query || "";
+  const genre = query.genre || "";
 
-    const searchQuery = query.query || "";
-    const genre = query.genre || "";
+  const options = {};
 
-    const options = {}
+  //Build search filters
+  if (searchQuery && searchQuery !== "allContents")
+    options.title = { $regex: searchQuery, $options: "i" };
+  if (genre && genre !== "allGenres") options.genre = genre;
 
-    //Build search filters
-    if(searchQuery)
-        options.title  = {  $regex: searchQuery, $options: 'i'  }
-    if (genre)
-        options.genre = genre;
+  //Define sorting options
+  // const sortOrder =
+  //   order === "lowest"
+  //     ? { price: 1 }
+  //     : order === "highest"
+  //     ? { price: -1 }
+  //     : order === "toprated"
+  //     ? { rating: 1 }
+  //     : order === "newest"
+  //     ? { createdAt: -1 }
+  //     : { _id: -1 };
 
-    //Define sorting options
-    // const sortOrder =
-    //   order === "lowest"
-    //     ? { price: 1 }
-    //     : order === "highest"
-    //     ? { price: -1 }
-    //     : order === "toprated"  
-    //     ? { rating: 1 }
-    //     : order === "newest"
-    //     ? { createdAt: -1 }
-    //     : { _id: -1 };
+  return { options };
+};
 
-  return { options }; 
-}
-
-export default GetSearchFilter
+export default GetSearchFilter;
