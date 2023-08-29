bd41e96ed42fddcbee5e49e0d7e2db92e416f37e
Alex Ignatenko <alex80850@gmail.com>
2023-08-29 16:37:34 +0300

    latest

--- Client/src/components/SliderItem/SliderItem.jsx
+++ Client/src/components/SliderItem/SliderItem.jsx
@@ -1,87 +1,112 @@
-import { useState, useEffect, useContext } from 'react'
-import ReactPlayer from 'react-player'
-import "./SliderItem.scss"
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useContext } from "react";
+import ReactPlayer from "react-player";
+import "./SliderItem.scss";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { authContext } from "../../context/authContext";
 import { UPDATE_USERLIST } from "../../context/reducerActions";
 
-const SliderItem = ({content}) => {
+const SliderItem = ({ content }) => {
+  const [isHover, setIsHover] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const { userInfo, userList, dispatch } = useContext(authContext);
 
-const [isHover, setIsHover] = useState(false);
-const [isPlaying, setIsPlaying] = useState(false);
-const {userInfo, userList, dispatch } = useContext(authContext);
-const [isInUserList, setIsInUserList] = useState(false);
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-
-useEffect(()  => {
-
-  const isInUserListQuery = async () => {
+  const addToUserList = async () => {
+    console.log(userInfo.token);
     try {
-      const path = `/user/isinuserlist/${content._id}`
-      const response = await axios.get(path, {
-        headers: {
-          authorization: userInfo.token,
-        },});
-        setIsInUserList(response.data);
-     } catch (error) {
-        console.log(error);
-     }
-  }
-  isInUserListQuery();
-},[isInUserList]);
-
-const addToUserList = async () => {
-  dispatch({type: UPDATE_USERLIST, payload: content._id});
-  try {
-    const response = await axios.put("user/updateuserlist:id", {
-      headers: {
-        authorization: userInfo.token,
-      },});
-  } catch (error) {
-    
-  }
-}
+      const response = await axios.post(
+        `users/updateuserlist/${content._id}`,
+        {},
+        {
+          headers: {
+            authorization: userInfo.token,
+          },
+        }
+      );
+      dispatch({ type: UPDATE_USERLIST, payload: response.data });
+    } catch (error) {}
+  };
 
   return (
-    <div className={`slide-item-container`}
+    <div
+      className={`slide-item-container`}
       onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => {setIsHover(false); setIsPlaying(false)}}
+      onMouseLeave={() => {
+        setIsHover(false);
+        setIsPlaying(false);
+      }}
     >
-        <img src={content.imgThumb} className='details-img-1' style={{display: !isHover ? 'block': "none"}}/>
-        <div className='container' style={{display: isHover ? 'block': "none"}}>
-          <div className='player-or-img'>
-            { !isPlaying ? (
-                <div className='img-container'>
-                  <img src={content.imgThumb} className="detials-img-2"/>
-                </div>
+      <img
+        src={content.imgThumb}
+        className="details-img-1"
+        style={{ display: !isHover ? "block" : "none" }}
+      />
+      <div
+        className="container"
+        style={{ display: isHover ? "block" : "none" }}
+      >
+        <div className="player-or-img">
+          {!isPlaying ? (
+            <div className="img-container">
+              <img src={content.imgThumb} className="detials-img-2" />
+            </div>
+          ) : (
+            <div className="player-container">
+              <ReactPlayer
+                url={content.movie}
+                playing={isPlaying}
+                height="100%"
+                width="100%"
+                className="player"
+              />
+            </div>
+          )}
+        </div>
+        <div className="detials">
+          <div className="detials-row1">
+            <div className="btn-col1">
+              <span
+                className="material-symbols-outlined btn-icon"
+                onClick={() => {
+                  setIsPlaying(!isPlaying);
+                }}
+              >
+                {!isPlaying ? "play_circle" : "stop_circle"}
+              </span>
+              {userList &&
+              userList.contents &&
+              userList.contents.find((c) => c._id === content._id) ? (
+                <span className="material-symbols-outlined btn-icon">
+                  do_not_disturb_on
+                </span>
               ) : (
-                <div className='player-container'>
-                  <ReactPlayer url={content.movie} playing={isPlaying} height="100%" width="100%" className="player" />
-                </div>
-              )
-            }
-
+                <span
+                  className="material-symbols-outlined btn-icon"
+                  onClick={() => addToUserList()}
+                >
+                  add_circle
+                </span>
+              )}
+            </div>
+            <div className="btn-col2">
+              <span
+                className="material-symbols-outlined btn-icon"
+                onClick={() => navigate(`/info/${content._id}`)}
+              >
+                info
+              </span>
+            </div>
           </div>
-          <div className="detials">
-            <div className="detials-row1">
-              <div className="btn-col1">
-                <span className="material-symbols-outlined btn-icon" onClick={()=>{setIsPlaying(!isPlaying)}}>{!isPlaying ? "play_circle" : "stop_circle"}</span>
-                <span className="material-symbols-outlined btn-icon" onClick={addToUserList}>add_circle</span>
-              </div>
-              <div className="btn-col2">
-                <span className="material-symbols-outlined btn-icon" onClick={() =>navigate(`/info/${content._id}`)}>info</span>
-              </div>
-            </div>
-            <div className="detials-row2">
-              <span>{content.duration}</span>
-              <span>{content.year}</span>
-              <span className='age-span'>+{content.limit}</span>
-              <span>{content.genre}</span>
-            </div>
+          <div className="detials-row2">
+            <span>{content.duration}</span>
+            <span>{content.year}</span>
+            <span className="age-span">+{content.limit}</span>
+            <span>{content.genre}</span>
           </div>
         </div>
+      </div>
     </div>
   );
 };
--- Server/Models/ListModel.js
+++ Server/Models/ListModel.js
@@ -1,15 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
 const listSchema = mongoose.Schema(
-    {
-        name : { type: String, required: true },
-        genre : { type: String},
-        type : { type: String},
-        contents: [{ type: Schema.Types.ObjectId, ref: "Content"}],
-    },
-    { timestamps: true }
+  {
+    name: { type: String, required: true },
+    contents: [{ type: Schema.Types.ObjectId, ref: "Content" }],
+  },
+  { timestamps: true }
 );
 
 const List = mongoose.model("List", listSchema);
 export default List;
-
